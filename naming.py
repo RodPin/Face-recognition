8b03d290225c906ab30314d7b69340622b329698
rodpin <prodrigo@poli.ufrj.br>
2021-01-17 13:38:06 -0300

    remove comments

--- naming.py
+++ naming.py
@@ -28,21 +28,6 @@ for person in persons:
 face_locations = face_recognition.face_locations(image)
 print()
 
- 
-#rodrigoIMG = face_recognition.load_image_file("./People I know/rodrigo.jpg")
-# unknown_image = face_recognition.load_image_file("unknown.jpg")
-
-# biden_encoding = face_recognition.face_encodings(known_image)[0]
-# unknown_encoding = face_recognition.face_encodings(image)[0]
-
-# try:
-#     rodrigo_face_encoding = face_recognition.face_encodings(rodrigoIMG)[0]
-#     # obama_sface_encoding = face_recognition.face_encodings(obama_image)[0]
-# except IndexError:
-#     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-#     quit()
-
-
 def writeSquare(eachFaceEncode,name,recognized):
     if name != 'Unknown':
         print('Person found: '+name)
@@ -74,7 +59,7 @@ def getColor(boolean):
 
 aux=[]
 for i, face in enumerate(unknown_face_encoding):
-    #COMPARA TODAS A FACES DADAS, COM CADA FACE RECONHECIDA NA FOTO PRINCIPAL
+    #Compare the faces in the folder of people we know with the ones in the image we're looking for 
     aux=face_recognition.compare_faces(personsFaceEncoding, unknown_face_encoding[i])
     if True in aux:
         writeSquare(face_locations[i],personsName[aux.index(True)],True)
@@ -82,10 +67,7 @@ for i, face in enumerate(unknown_face_encoding):
         writeSquare(face_locations[i],"Unknown",False)
             
     
-# print(face_locations)
-cv2.imshow("Minha Imagem", image)
 cv2.imwrite(join('./', 'processed.jpg'), image)
-k = cv2.waitKey(0)
 print()
 print('End...')
 
